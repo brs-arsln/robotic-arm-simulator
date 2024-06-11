5c5c1cc68b420fc0c9d5a9bbae33f684015955db
brs-arsln <brsarsln3091@gmail.com>
2024-06-11 18:35:15 +0300

    done

---
+++ Robotic Arm Project/Assets/Scripts/CubeDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeDestroy : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("coloredCube"))
+        {
+            StartCoroutine(DestroyCube());
+        }
+        IEnumerator DestroyCube()
+        {
+            yield return new WaitForSeconds(5);
+            Destroy(other.gameObject);
+        }
+    }
+}
